a01882e626fb4e2efc84c96ff5ac611599d6c102
Lagomorphism <wonbinlee@gmail.com>
2013-09-15 22:19:39 -0400

    Revert "Initial commit."
    This reverts commit c9528696265301dba8c428b1f727f08fb3b22aad.

--- server.js
+++
@@ -1,6 +0,0 @@
-var util = require('util'), connect = require('connect'), port = 4567;
-
-connect.createServer(connect.static(__dirname)).listen(port);
-
-util.puts('Listening  on ' + port + '...');
-util.puts('Press CTRL + C to stop.');
